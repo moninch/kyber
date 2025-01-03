f70b46133f6952188001a3abf2c5e4e5a1e9fed6
Даниил <mon.inch@yandex.ru>
2025-01-03 13:42:24 +0300

    delete redis info

--- app/config.py
+++ app/config.py
@@ -13,23 +13,19 @@ class Settings(BaseSettings):
     DB_NAME: str
     DATABASE_URL: str = None
 
-    REDIS_HOST: str
-    REDIS_PORT: int
+    SECRET_KEY: str
+    ALGORITHM: str
 
-    SECRET_KEY : str
-    ALGORITHM : str
-
-    @model_validator(mode='before')
+    @model_validator(mode="before")
     @classmethod
     def get_database_url(cls, values):
-        values["DATABASE_URL"] = f"postgresql+asyncpg://{values['DB_USER']}:{values['DB_PASS']}@{values['DB_HOST']}:{values['DB_PORT']}/{values['DB_NAME']}"
+        values["DATABASE_URL"] = (
+            f"postgresql+asyncpg://{values['DB_USER']}:{values['DB_PASS']}@{values['DB_HOST']}:{values['DB_PORT']}/{values['DB_NAME']}"
+        )
         return values
 
     class Config:
         env_file = ".env"
 
 
-
 settings = Settings()
-
-
